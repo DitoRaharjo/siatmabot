51a98462e8fb9102b777ae54c482e54448d871bc
Dito Raharjo <ditoraharjo@gmail.com>
2017-02-16 00:29:14 +0700

    Login-Register & Dashboard

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
+use Carbon\Carbon;
+
+use DB;
+
+use App\User;
+use App\Prodi;
+use App\Fakultas;
+
+class UserController extends Controller
+{
+    public function dashboardAdmin() {
+      if (strcasecmp(Auth::user()->role,'admin')==0) {
+        return view('front.dashboard.admin');
+      } else {
+        alert()->error('Akun anda tidak memiliki hak untuk melihat halaman ini', 'Pelanggaran Akun!');
+        return redirect()->route('dashboard.mahasiswa');
+      }
+    }
+
+    public function dashboardMahasiswa() {
+      if (strcasecmp(Auth::user()->role,'admin')==0 || strcasecmp(Auth::user()->role,'mahasiswa')==0) {
+        return view('front.dashboard.mahasiswa');
+      } else {
+        alert()->error('Akun anda tidak memiliki hak untuk melihat halaman ini', 'Pelanggaran Akun!');
+        return redirect()->route('user.login');
+      }
+    }
+
+    public function register() {
+      $semuaProdi = Prodi::all();
+      $semuaFakultas = Fakultas::all();
+
+      return view('front.loginregister.register', compact('semuaProdi', 'semuaFakultas'));
+    }
+
+    public function doRegister(Request $request) {
+      $this->validate($request, [
+        'telegram_username' => 'required',
+        'fullname' => 'required',
+        'npm' => 'required',
+        'prodi_id' => 'required',
+        'email' => 'required|email',
+        'password' => 'required'
+      ]);
+
+      $user_data = $request->except('_token');
+
+      $fakultasId = Prodi::find($user_data['prodi_id'])->fakultas->id;
+
+      $user_data['fakultas_id'] = $fakultasId;
+      $user_data['password'] = bcrypt($user_data['password']);
+      $user_data['role'] = "Mahasiswa";
+      $user_data['registerdate'] = Carbon::now();
+
+      DB::beginTransaction();
+
+      try {
+        User::create($user_data);
+
+        DB::commit();
+
+        alert()->success('Akun anda berhasil di register', 'Berhasil!');
+        return redirect()->route('dashboard.mahasiswa');
+      } catch (\Exception $e) {
+        DB::rollback();
+
+        throw $e;
+      }
+    }
+
+    public function login()
+    {
+      if (Auth::check()) {
+        if(Auth::user()->status == 1) {
+          if(Auth::user()->deleted_at == NULL) {
+            if (strcasecmp(Auth::user()->role,'mahasiswa')==0)
+            {
+              $userlogin = Auth::user();
+              return redirect()->route('dashboard.mahasiswa');
+            } else if (strcasecmp(Auth::user()->role,'admin')==0) {
+              $userlogin = Auth::user();
+              return redirect()->route('dashboard.admin');
+            } else {
+                alert()->error('Maaf Role anda belum ditentukan', 'Gagal Login!');
+                Auth::logout();
+                return redirect()->route('user.login');
+            }
+          }
+          else {
+            Auth::logout();
+            alert()->error('Mohon maaf akun anda telah di HAPUS oleh admin, silahkan hubungi admin', 'Akun Dihapus!');
+            return redirect()->route('user.login');
+          }
+        }
+        else {
+          Auth::logout();
+          alert()->error('Mohon maaf akun anda telah di NON-AKTIFKAN oleh admin, silahkan hubungi admin', 'Akun Non-Aktif!');
+          return redirect()->route('user.login');
+        }
+      }
+      else
+      {
+        $semuaProdi = Prodi::all();
+        $semuaFakultas = Fakultas::all();
+
+        return view('front.loginregister.login', compact('semuaProdi', 'semuaFakultas'));
+      }
+    }
+
+    public function doLogin(Request $request)
+    {
+      $this->validate($request, [
+          'email' => 'required|email',
+          'password' => 'required'
+      ]);
+
+      $user_data = $request->except('_token');
+
+      if($this->checkLupaPass($user_data) == false)
+      {
+        if(Auth::attempt($user_data))
+        {
+          if(Auth::user()->status == 1) {
+            if(Auth::user()->deleted_at == NULL) {
+              if (strcasecmp(Auth::user()->role,'mahasiswa')==0) {
+                $userlogin = Auth::user();
+                return redirect()->route('dashboard.mahasiswa');
+              } else if(strcasecmp(Auth::user()->role,'admin')==0) {
+                $userlogin = Auth::user();
+                return redirect()->route('dashboard.admin');
+              } else {
+                alert()->error('Maaf Role anda belum ditentukan', 'Gagal Login!');
+                Auth::logout();
+                return redirect()->route('user.login');
+              }
+            }
+            else {
+              Auth::logout();
+              alert()->error('Mohon maaf akun anda telah di HAPUS oleh admin, silahkan hubungi admin', 'Akun Dihapus!');
+              return redirect()->route('user.login');
+            }
+          }
+          else {
+            Auth::logout();
+            alert()->error('Mohon maaf akun anda telah di NON-AKTIFKAN oleh admin, silahkan hubungi admin', 'Akun Non-Aktif!');
+            return redirect()->route('user.login');
+          }
+        }
+        else
+        {
+          alert()->error('Username atau Password anda salah', 'Gagal Login!');
+          return redirect()->route('user.login');
+        }
+      }
+      else {
+        $emailUser = $user_data['email'];
+        return view('front.lupapass.index', compact('emailUser'));
+      }
+    }
+
+    public function checkLupaPass($user_data)
+    {
+      $userCount = User::where('email', '=', $user_data['email'])->count();
+      $user = User::where('email', '=', $user_data['email'])->get();
+
+      if($userCount == 0) {
+        return false;
+      }
+      else {
+        if($user[0]->lupa_pass != NULL) {
+          return true;
+        }
+      }
+    }
+
+    public function gantiPassDo(Request $request)
+    {
+      $this->validate($request, [
+          'email' => 'required|email',
+          'password' => 'required',
+          'password_confirmation' => 'required',
+      ]);
+
+      $user_data = $request->except('_token');
+
+      DB::beginTransaction();
+
+      try {
+        $userId = User::select('id')->where('email', '=', $user_data['email'])->get();
+
+        $user = User::find($userId[0]->id);
+
+        $passwordBaru = bcrypt($user_data['password']);
+
+        $user->password = $passwordBaru;
+        $user->lupa_pass = NULL;
+        $user->save();
+
+        DB::commit();
+      } catch (\Exception $e) {
+        DB::rollback();
+
+        throw $e;
+      }
+
+      $this->doLogin($request);
+
+      alert()->success('Password berhasil diperbaharui', 'Berhasil!');
+      return redirect()->route('user.login');
+    }
+
+    public function doLogout()
+    {
+      Auth::logout();
+
+      alert()->success('Berhasil Log Out', 'Log Out!');
+      return redirect()->route('user.login');
+    }
+}
---
+++ laravel/resources/views/front/loginregister/register.blade.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Seputar Farmasi : Informasi dan Konsultasi Kesehatan" />
+  	<meta name="author" content="Seputar Farmasi" />
+
+    <title>SIATMA Bot</title>
+    <link rel="icon" href="{{ asset('images/logo_tabbrowser.png') }}" type="image" sizes="16x16"> <!------------------------------------------ ICON-------------->
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('Template/AdminPage/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{ asset('Template/AdminPage/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="{{ asset('Template/AdminPage/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="{{ asset('Template/AdminPage/vendors/animate.css/animate.min.css') }}" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="{{ asset('Template/AdminPage/build/css/custom.min.css') }}" rel="stylesheet">
+    <!-- Sweet Alert -->
+    <link href="{{ asset('css/sweetalert/sweetalert2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sweetalert/sweetalert2.css') }}" rel="stylesheet">
+
+
+  </head>
+
+  <body class="login">
+    <div>
+
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+
+      <div class="login_wrapper">
+
+        <div class="animate form login_form" >
+          <section class="login_content">
+            <form action="{{ route('user.do.register') }}" method="post">
+              {{ csrf_field() }}
+              <h1>Create Account</h1>
+              <div>
+                <input name="telegram_username" type="text" class="form-control" placeholder="Telegram Username, ex : vincentiusdito, Without @ " required="" />
+              </div>
+              <div>
+                <input name="fullname" type="text" class="form-control" placeholder="Fullname" required="" />
+              </div>
+              <div>
+                <input name="npm" type="number" class="form-control" placeholder="NPM" required="" />
+              </div>
+              <div>
+                </br>
+              </div>
+              <div>
+                <select class="select2_group form-control" required="" name="prodi_id">
+                  @foreach($semuaFakultas as $fakultas)
+                  <optgroup label="{{ $fakultas->nama }}">
+                            @foreach($semuaProdi as $prodi)
+                              @if($prodi->fakultas_id == $fakultas->id)
+                              <option value="{{ $prodi->id }}">{{ $prodi->nama }}</option>
+                              @endif
+                            @endforeach
+                  </optgroup>
+                  @endforeach
+                </select>
+              </div>
+              <div>
+                </br>
+              </div>
+              <div>
+                <input name="email" type="email" class="form-control" placeholder="Email" required="" />
+              </div>
+              <div>
+                <input name="password" id="password" type="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                <input name="password_confirmation" id="password_confirmation" type="password" class="form-control" placeholder="Password Confirmation" required="" />
+              </div>
+              <div>
+                <button type="submit" class="btn btn-default submit" >Register</button>
+              </div>
+
+              <div class="clearfix"></div>
+
+
+              <div class="separator">
+                <p class="change_link">Already a member ?
+                  <a href="#signup" class="to_register"> Log in </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+
+                </div>
+              </div>
+
+            </form>
+          </section>
+        </div>
+
+        <div id="register" class="animate form registration_form">
+          <section class="login_content">
+
+            <form action="{{ route('user.auth.login') }}" method="post">
+              {{ csrf_field() }}
+              <h1>SIATMA Bot Login</h1>
+              <div>
+                <input type="email" name="email" class="form-control" placeholder="Email" required="" />
+              </div>
+              <div>
+                <input type="password" name="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div class="clearfix"></div>
+
+
+              <div class="separator">
+                <p class="change_link">New to site?
+                  <a href="#signin" class="to_register"> Create Account </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+
+                </div>
+              </div>
+
+            <button type="submit" class="btn btn-default submit" >Log in</button>
+            <a class="reset_pass" href="#">Lupa password?</a>
+            </form>
+          </section>
+        </div>
+
+      </div>
+    </div>
+
+
+    <!-- Select2 -->
+    <script>
+      $(document).ready(function() {
+        $(".select2_group").select2({});
+        $(".select2_multiple").select2({
+          maximumSelectionLength: 0,
+          placeholder: "Silahkan pilih kategori yang sesuai dengan artikel",
+          allowClear: true
+        });
+      });
+    </script>
+    <!-- /Select2 -->
+
+    <!-- Untuk Pencocokan Konfirmasi Password -->
+    <script>
+      var password = document.getElementById("password")
+      , confirm_password = document.getElementById("password_confirmation");
+
+      function validatePassword(){
+      if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+      } else {
+        confirm_password.setCustomValidity('');
+      }
+      }
+
+      password.onchange = validatePassword;
+      confirm_password.onkeyup = validatePassword;
+    </script>
+    <!-- Untuk Pencocokan Konfirmasi Password -->
+
+    <!--<script src="{{ asset('js/loginadmin/index.js') }}"></script>-->
+    <!-- Sweet Alert -->
+    <script src="{{ asset('js/sweetalert/sweetalert2.js') }}"></script>
+
+    <!-- Include this after the sweet alert js file -->
+    @include('sweet::alert')
+
+  </body>
+</html>
