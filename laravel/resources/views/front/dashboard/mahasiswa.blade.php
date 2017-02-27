52e375fddbcc52f582d0e481fd6e331dcdd839fe
Dito Raharjo <ditoraharjo@gmail.com>
2017-02-28 01:30:06 +0700

    Under Maintenance "Dashboard Mahasiswa"

---
+++ laravel/resources/views/front/dashboard/mahasiswa.blade.php
@@ -0,0 +1,13 @@
+@extends('front.template')
+
+@section('content')
+
+<div class="right_col" role="main">
+  <h1><b>Under maintenance, please be patient</b></h1>
+</div>
+
+@endsection
+
+@section('custom_script')
+
+@endsection
