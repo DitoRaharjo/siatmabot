041dc3c5b9138439833b9dbc64d5310c5ef0228c
Dito Raharjo <ditoraharjo@gmail.com>
2017-03-02 17:37:41 +0700

    Testing LINE API 2.1

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -49,11 +49,11 @@ class LineBotController extends Controller
     			if($event['message']['type'] == 'text')
     			{
     				// send same message as reply to user
-    				$result = $bot->replyText($event['replyToken'], $event['message']['text']);
+    				// $result = $bot->replyText($event['replyToken'], $event['message']['text']);
 
     				// or we can use pushMessage() instead to send reply message
-    				// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event['message']['text']);
-    				// $result = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
+    				$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event['message']['text']);
+    				$result = $bot->pushMessage($event['source']['userId'], $textMessageBuilder);
 
     				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
     			}
