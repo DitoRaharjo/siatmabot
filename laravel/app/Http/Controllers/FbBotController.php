ba4d4c80bd99be5cc95edc5a0c15701c5dae6f81
Dito Raharjo <ditoraharjo@gmail.com>
2017-03-03 01:04:37 +0700

    Testing Facebook Messenger 1.8

--- laravel/app/Http/Controllers/FbBotController.php
+++ laravel/app/Http/Controllers/FbBotController.php
@@ -37,23 +37,31 @@ class FbBotController extends Controller
       // $responses_convert = json_decode($responses);
       //
       // $userId = $responses_convert->entry[0]->messaging[0]->sender->id;
+      //
+      // $data = array(
+      //   'recipient'=>array('id'=>"1334082683305106"),
+      //   'message'=>array('text'=>"Halo juga")
+      // );
+      //
+      // $opts = array(
+      //   'http'=>array(
+      //     'method'=>'POST',
+      //     'content'=>json_encode($data),
+      //     'header'=>"Content-Type: application/json\n"
+      //   )
+      // );
+      // $context = stream_context_create($opts);
+      //
+      // $token = "EAAbbj6niWKABAM9MAdxj9B4v7ZAm9faW1ZAzp5sGpZCepxWQEzAmOyGlMBPlNXinomjamNmhlJaiumtLsh12eWbsn9LDtzEaMKxY3JJUWKIiOhFoi7FvWoW4ShxbZCyibEBylJ1XP0UVQTMCTh0ZCu2oQ38RRSSe7BHa2nSfPfQZDZD";
+      // $website = "https://graph.facebook.com/v2.6/me/messages?access_token=$token";
+      // file_get_contents($website, false, $context);
 
-      $data = array(
-        'recipient'=>array('id'=>"1334082683305106"),
-        'message'=>array('text'=>"Halo juga")
-      );
-
-      $opts = array(
-        'http'=>array(
-          'method'=>'POST',
-          'content'=>json_encode($data),
-          'header'=>"Content-Type: application/json\n"
-        )
-      );
-      $context = stream_context_create($opts);
-
-      $token = "EAAbbj6niWKABAM9MAdxj9B4v7ZAm9faW1ZAzp5sGpZCepxWQEzAmOyGlMBPlNXinomjamNmhlJaiumtLsh12eWbsn9LDtzEaMKxY3JJUWKIiOhFoi7FvWoW4ShxbZCyibEBylJ1XP0UVQTMCTh0ZCu2oQ38RRSSe7BHa2nSfPfQZDZD";
-      $website = "https://graph.facebook.com/v2.6/me/messages?access_token=$token";
-      file_get_contents($website, false, $context);
+      $chatId = 253128578;
+      $text = "Chat FB Masuk";
+      
+      Telegram::sendMessage([
+        'chat_id' => $chatId,
+        'text' => $text,
+      ]);
     }
 }
