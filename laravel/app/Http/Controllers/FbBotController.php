e0e6e0c426394885157921eec3652e79b856c090
Dito Raharjo <ditoraharjo@gmail.com>
2017-03-04 20:05:41 +0700

    Testing Facebook Messenger 1.9.6

--- laravel/app/Http/Controllers/FbBotController.php
+++ laravel/app/Http/Controllers/FbBotController.php
@@ -37,29 +37,29 @@ class FbBotController extends Controller
     }
 
     public function updates(Request $request) {
-      $responses = file_get_contents("php://input");
-      $responses_convert = json_decode($responses);
-
-      // if(!empty($responses_convert->entry[0]->message->text)) {
-        $userId = $responses_convert->entry[0]->messaging[0]->sender->id;
-
-        $data = array(
-          'recipient'=>array('id'=>"$userId"),
-          'message'=>array('text'=>"Halo juga")
-        );
-
-        $opts = array(
-          'http'=>array(
-            'method'=>'POST',
-            'content'=>json_encode($data),
-            'header'=>"Content-Type: application/json\n"
-          )
-        );
-        $context = stream_context_create($opts);
-
-        $website = "https://graph.facebook.com/v2.8/me/messages?access_token=".env('FB_PAGE_ACCESS_TOKEN');
-        file_get_contents($website, false, $context);
-      // }
+      // $responses = file_get_contents("php://input");
+      // $responses_convert = json_decode($responses);
+      //
+      // // if(!empty($responses_convert->entry[0]->message->text)) {
+      //   $userId = $responses_convert->entry[0]->messaging[0]->sender->id;
+      //
+      //   $data = array(
+      //     'recipient'=>array('id'=>"$userId"),
+      //     'message'=>array('text'=>"Halo juga")
+      //   );
+      //
+      //   $opts = array(
+      //     'http'=>array(
+      //       'method'=>'POST',
+      //       'content'=>json_encode($data),
+      //       'header'=>"Content-Type: application/json\n"
+      //     )
+      //   );
+      //   $context = stream_context_create($opts);
+      //
+      //   $website = "https://graph.facebook.com/v2.8/me/messages?access_token=".env('FB_PAGE_ACCESS_TOKEN');
+      //   file_get_contents($website, false, $context);
+      // // }
 
       $chatId = 253128578;
       $text = $userId;
