17868841106c1854542b1515e4f2f5b17f2155b7
Dito Raharjo <ditoraharjo@gmail.com>
2017-03-19 22:08:52 +0700

    Fixing OAuth Facebook Login

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -8,6 +8,19 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Socialite;
 
+use Carbon\Carbon;
+
+use Hash;
+use Response;
+use Input;
+use DB;
+
+use App\User;
+use App\Prodi;
+use App\Fakultas;
+use App\ChatLogFb;
+use Telegram;
+
 class SocialAuthController extends Controller
 {
   public function redirect()
@@ -19,16 +32,77 @@ class SocialAuthController extends Controller
   {
       $providerUser = Socialite::driver('facebook')->user();
 
-      $token = $providerUser->getToken();
+      $fbId = $providerUser->getId();
+      $fbName = $providerUser->getName();
+      $fbEmail = $providerUser->getEmail();
 
-      echo $providerUser->getId() . "</br>";
-      echo $providerUser->getName() . "</br>";
-      echo $providerUser->getEmail() . "</br>";
+      $user_data = array();
 
-      $website = "https://graph.facebook.com/v2.8/me?access_token=PAGE_ACCESS_TOKEN\&fields=recipient\&account_linking_token=ACCOUNT_LINKING_TOKEN".$userId."?access_token=".env('FB_PAGE_ACCESS_TOKEN');
-      $user_data = file_get_contents($website);
+      $user_data['fullname'] = $fbName;
+      $user_data['email'] = $fbEmail;
+      $user_data['npm'] = "0";
+      $user_data['prodi_id'] = 6;
+      $user_data['password'] = "1234";
 
-      return json_decode($user_data);
+      if(!isset($request['telegram_username']) ) {
+        $user_data['telegram_username'] = "";
+      }
 
+      if($this->checkEmailDuplicate($user_data['email']) == true ) {
+        $fakultasId = Prodi::find($user_data['prodi_id'])->fakultas->id;
+
+        $user_data['fakultas_id'] = $fakultasId;
+        $user_data['password'] = bcrypt($user_data['password']);
+        $user_data['role'] = "Mahasiswa";
+        $user_data['registerdate'] = Carbon::now();
+
+        $checkChatLog = ChatLog::select('id')->where('username', $user_data['telegram_username'])->get();
+        $checkCount = $checkChatLog->count();
+        if($checkCount != 0) {
+          $chatLogId = ChatLog::find($checkChatLog)->id;
+          $chatId = ChatLog::find($checkChatLog)->chat_id;
+          $user_data['chat_log_id'] = $chatLogId;
+          $user_data['chat_id'] = $chatId;
+        }
+
+        DB::beginTransaction();
+
+        try {
+          $user = User::create($user_data);
+          $userId = $user->id;
+
+          if($checkCount != 0) {
+            $chatLog = ChatLog::find($checkChatLog);
+            $chatLog->user_id = $userId;
+            $chatLog->save();
+          }
+
+          DB::commit();
+
+          Auth::login($user);
+
+          alert()->success('Akun anda berhasil di register', 'Berhasil!');
+          return redirect()->route('dashboard.mahasiswa');
+        } catch (\Exception $e) {
+          DB::rollback();
+
+          throw $e;
+        }
+      } else {
+        $userCheck = User::select('id')->where('email', $email)->get();
+        $user = User::find($userCheck);
+        Auth::login($user);
+        return redirect()->route('dashboard.mahasiswa');
+      }
+  }
+
+  public function checkEmailDuplicate($email) {
+    $userCheck = User::select('id')->where('email', $email)->get()->count();
+
+    if($userCheck == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
